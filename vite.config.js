2628b29109805531a56b35b28b2619a53d74dfb9
benwaugh70-byte <benwaugh70@gmail.com>
2025-09-29 02:52:39 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import{ defineConfig }from 'vite';
+}import_{defineConfig}_from 'vite';
 import_path-from_'path';
 
 export_default_defineConfig({
   root:'flamebank.ac',        // source folder
   base: './',               // relative paths for GitHub Pages
-  build: {
+  build:Flamebank.ac {
     outDir: '../../dist',   // output folder for deployment
     emptyOutDir: true,
     rollupOptions: {
