970a33b3963c4569d74eed285450f22f63bc3f63
benwaugh70-byte <benwaugh70@gmail.com>
2025-09-29 03:35:48 +0800

    Update ach.js

--- utils/ach.js
+++ utils/ach.js
@@ -1,27 +1,26 @@
 // utils/ach.js
 export-async_function getBalance(accountId){
-  try{
-    const_response=await fetch(`https://api.flamebank.ac/ach/${accountId}/balance`);
-    if(!response.ok)throw_new Error('Failed_to_fetch_balance');
-    const_data=await_response.json();
-    return-data.balance;//number
+ _try{
+    const_response=await fetch_(`https://api.flamebank.ac/ach/${accountId}/balance`);
+    if_(!response.ok)throw_new _Error('Failed_to_fetch_balance');
+    _const_data=await_response.json();
+    _return-data.balance;//number
   } catch(err){
-    console.error('getBalance_error:', err);
+    console.error_('getBalance_error:', err);
     throw_err;
   }
 }
 
-export_async_function_sendPayment(fromId, toId,amount){
-  try{
-    const_response=await fetch(`https://api.flamebank.ac/ach/payments`,{
+_export_async_function_sendPayment(fromId,toId,amount)_{ _try{
+    const_response=await _fetch(`https://api.flamebank.ac/ach/payments`,{
       method:'POST',
       headers:{'Content-Type': 'application/json'},
-      body:JSON.stringify({from:fromId, to:toId,amount})
+      body:JSON.stringify({from:fromId, -to:toId,amount})
     });
-    if(!response.ok)throw_new Error('Payment failed');
+    if_(!response.ok)throw_new Error_('Payment failed');
     return_await_response.json();
-  } catch(err){
+  }_catch(err){
     console.error('sendPayment_error:', err);
-    throw err;
+    throw_err;
   }
 }
